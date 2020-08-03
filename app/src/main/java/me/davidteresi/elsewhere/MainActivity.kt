8a67cff27d042ff608fdb138af169118d57e9f4e
David Teresi <dkteresi+github@gmail.com>
2020-08-03 00:05:33 -0400

    transparent system bars

--- app/src/main/java/me/davidteresi/elsewhere/MainActivity.kt
+++ app/src/main/java/me/davidteresi/elsewhere/MainActivity.kt
@@ -2,10 +2,15 @@ package me.davidteresi.elsewhere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val view = window.decorView
+        view.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
     }
 }
