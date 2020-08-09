2a6d860a6ada3e58344b10457ba7e45017ade9c1
David Teresi <dkteresi+github@gmail.com>
2020-08-09 16:29:06 -0400

    hook up map button

--- app/src/main/java/me/davidteresi/elsewhere/MainActivity.kt
+++ app/src/main/java/me/davidteresi/elsewhere/MainActivity.kt
@@ -2,11 +2,13 @@ package me.davidteresi.elsewhere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -21,6 +23,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.target.CustomViewTarget
 import com.bumptech.glide.request.transition.Transition
+import com.google.android.material.chip.Chip
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.stream.JsonReader
@@ -44,11 +47,27 @@ class MainActivity : AppCompatActivity() {
         val view = window.decorView
         view.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+
+        val wikipediaChip = findViewById<Chip>(R.id.wikipedia_chip)
+        val mapChip = findViewById<Chip>(R.id.map_chip)
+
+        wikipediaChip.setOnClickListener { onWikipediaChipClick() }
+        mapChip.setOnClickListener { onMapChipClick() }
     }
 
     override fun onResume() {
         super.onResume()
         refresh()
+    }
+
+    fun onMapChipClick() {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse("geo:${place.coord.lat},${place.coord.lon}")
+        startActivity(intent)
+    }
+
+    fun onWikipediaChipClick() {
+
     }
 
     private fun refresh() {
