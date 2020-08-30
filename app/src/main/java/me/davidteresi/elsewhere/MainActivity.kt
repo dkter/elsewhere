bbbb205c477aab392b17be35228a63f72ffc0187
David Teresi <dkteresi@gmail.com>
2020-08-30 14:09:32 -0400

    show loading icon when loading background image

--- app/src/main/java/me/davidteresi/elsewhere/MainActivity.kt
+++ app/src/main/java/me/davidteresi/elsewhere/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.WorkManager
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -19,6 +20,7 @@ import android.util.Log
 import android.view.View
 import android.view.ViewGroup.MarginLayoutParams
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextClock
 import android.widget.TextView
 import androidx.core.view.updateMargins
@@ -28,7 +30,11 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.RequestListener
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import com.google.gson.Gson
@@ -268,11 +274,35 @@ class MainActivity : AppCompatActivity() {
      * @param cacheOnly whether to only load the image from cache
      */
     private fun setImageBackground(cacheOnly: Boolean = false) {
+        val loadingIcon = findViewById<RelativeLayout>(R.id.loading_icon)
+        loadingIcon.visibility = View.VISIBLE
         Glide.with(this)
             .load(imageUrl)
             .onlyRetrieveFromCache(cacheOnly)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .placeholder(R.drawable.default_bg)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    loadingIcon.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    loadingIcon.visibility = View.GONE
+                    return false
+                }
+            })
             .into(findViewById<ImageView>(R.id.imageView))
     }
 
